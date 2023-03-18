6b0924d76416d489529241727b316aedacf02e0e
Sagar Ahirrao <sagar4ahirrao@gmail.com>
2023-03-18 18:28:37 +0530

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: "ugnzPfobQ0vnCi0-tdq5SFtUMR764EwbCJyZHBrbWVs"
+    }
+  },
+  "vercel-deploy": {
+    enabled: true,
+  },
+});
